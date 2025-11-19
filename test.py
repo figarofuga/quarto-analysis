495a631a99f215c73fe05412c3d39d8ba6f4ca85
figarofuga <61215157+figarofuga@users.noreply.github.com>
2025-11-19 13:43:21 +0000

    start dowhy

--- test.py
+++ test.py
@@ -1,8 +1,32 @@
 #%%
 
+# Required libraries
 import pandas as pd
-import polars as pl
-import tableone
-import marginaleffects
+
 
 #%%
+# I. Create a causal model from the data and domain knowledge.
+
+lalonde_pre = pd.read_csv("lalonde.csv")
+
+lalonde = (pd.get_dummies(lalonde_pre, 
+                         columns=["race"], dtype=int)
+           .drop(columns=['Unnamed: 0'])
+           
+           
+)
+
+#%%
+import dowhy
+from dowhy import CausalModel
+
+model = CausalModel(
+    data=lalonde,
+    treatment="treat",
+    outcome="re78",
+    common_causes=["age", "educ", "married", "nodegree", "re74", "re75", "race_black", "race_hispan", "race_white"],
+    instruments=None)
+
+#%%
+model.view_model()
+# %%
