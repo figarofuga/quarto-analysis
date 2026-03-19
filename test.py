fbf7664940f0a205822f4360490e92f91b4c2537
figarofuga <figarofuga@gmail.com>
2026-03-19 23:51:22 +0000

    start S-learner

--- test.py
+++ test.py
@@ -6,173 +6,38 @@ lalonde = (
     pd.read_csv("rawdata/lalonde.csv")
     .pipe(pd.get_dummies, columns=["race"], drop_first=True, dtype=int)
 )
-import pandas as pd
+
+from econml.metalearners import SLearner
 import numpy as np
-import statsmodels.formula.api as smf
+import pandas as pd 
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.model_selection import train_test_split
 
+X = lalonde.loc[:,["age", "married", "educ", "nodegree", "re74", "re75", "race_hispan", "race_white"]]
+y = lalonde['re78']
+T = lalonde['treat']
+n = lalonde.shape[0]
 
-fit_ols = smf.ols("re78 ~ treat + age + educ + race_hispan + race_white + married + nodegree + re74 + re75", data = lalonde)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, 
+    y, 
+    test_size=0.2, 
+    random_state=42, 
+    stratify=T
+    )
 
-res_ols = fit_ols.fit()
+T_train = T.loc[y_train.index]
+T_test = T.loc[y_test.index]
 
-coef_table = (
-    res_ols.summary2().tables[1]
-    .rename(columns={"Coef.": "Coef", "Std.Err.": "Std.Err", "P>|t|": "P"})
-    [["Coef", "Std.Err", "t", "P"]]
-    .round(2)
-)
+# Instantiate S learner
+overall_model = GradientBoostingRegressor(n_estimators=100, max_depth=6, min_samples_leaf=int(491/100))
+S_learner = SLearner(overall_model=overall_model)
+# Train S_learner
+S_learner.fit(y_train, T_train, X=X_train)
+# Estimate treatment effects on test data
+S_te = S_learner.effect(X_test)
 
-
-# %%
-
-# Required libraries
-import pandas as pd
-from causalml.propensity import LogisticRegressionPropensityModel
-from causalml.match import NearestNeighborMatch
-import pandas as pd
-import numpy as np
-
-
-lalonde_py = pd.read_csv("rawdata/lalonde.csv")
-# Assume X contains your features (covariates), 
-# y is the outcome variable (not used in PSM itself, but for effect estimation later),
-# and a is a binary indicator (0 or 1) for treatment assignment.
-# This data should be loaded into a pandas DataFrame or numpy array.
-
-# Example placeholder data
-X = lalonde_py[['age', 'educ', 'race', 'married', 'nodegree', 're74', 're75']] 
-a = lalonde_py['treat'] # 0 for control, 1 for treatment
-
-# 1. Estimate propensity scores
-# Use a Gradient Boosted Model to predict treatment assignment 'a' based on features 'X'
-from causalml.propensity import ElasticNetPropensityModel
-
-pm = ElasticNetPropensityModel(n_fold=5, random_state=42)
-ps = pm.fit_predict(X, a)
-
-from causalml.match import NearestNeighborMatch, create_table_one
-
-psm = NearestNeighborMatch(replace=False,
-                           ratio=1,
-                           random_state=42)
-matched = psm.match_by_group(data=lalonde_py,
-                             treatment_col='treat',
-                             score_cols=['propensity_score'],
-                             groupby_col=None)
-
-create_table_one(data=matched,
-                 treatment_col='treat',
-                 features=['age', 'educ', 'race', 'married', 'nodegree', 're74', 're75'])
-# Optional: Add propensity scores to your main DataFrame
-lalonde_py['propensity_score'] = propensity_scores
-
-
-# %%
-# I. Create a causal model from the data and domain knowledge.
-
-lalonde_pre = pd.read_csv("rawdata/lalonde.csv")
-
-lalonde = (pd.get_dummies(lalonde_pre, 
-                         columns=["race"], dtype=int)
-           
-           
-)
-
-
-# %%
-
-import dowhy
-import networkx as nx
-from dowhy import CausalModel
-
-
-# 2. NetworkXでグラフを定義
-causal_graph = nx.DiGraph()
-# "age", "educ", "married", "nodegree", "re74", "re75", "race_black", "race_hispan", "race_white"
-# ノードとエッジを追加 (交絡構造: Age -> Exercise, Age -> Health)
-causal_graph.add_nodes_from(["treat", "age", "educ", "married", "nodegree", "re74", "re75", "race_black", "race_hispan", "race_white", "re78"])
-causal_graph.add_edges_from([
-    ('treat', 're78'),
-    ('re74', 're75'),
-    ('age', 'treat'), 
-    ('age', 're78'),
-    ('educ', 'treat'),
-    ('educ', 're78'),
-    ('married', 'treat'),
-    ('married', 're78'),
-    ('nodegree', 'treat'),
-    ('nodegree', 're78'),
-    ('re74', 'treat'),
-    ('re75', 'treat'), 
-    ('re75', 're78'), 
-    ('race_black', 'treat'),
-    ('race_black', 're74'),
-    ('race_black', 're75'),
-    ('race_black', 're78'),
-    ('race_hispan', 'treat'),
-    ('race_hispan', 're74'),
-    ('race_hispan', 're75'),
-    ('race_hispan', 're78'),
-    ('race_white', 'treat'),
-    ('race_white', 're74'),
-    ('race_white', 're75'),
-    ('race_white', 're78')
-    
-])
-
-# 3. NetworkXオブジェクトをGML文字列に変換
-# 注意: 文字列内の改行コードなどを整形して渡します
-gml_string = "".join(nx.generate_gml(causal_graph))
-
-# 4. モデルの定義
-model = CausalModel(
-    data=lalonde,
-    treatment='treat',
-    outcome='re78',
-    graph=gml_string
-)
-
-# 5. グラフの確認
-model.view_model()
-
-
-
-# model = CausalModel(
-#     data=lalonde_post,
-#     treatment="treat",
-#     outcome="re78",
-#     common_causes=["age", "educ", "married", "nodegree", "re74", "re75", "race_black", "race_hispan", "race_white"],
-#     instruments=None)
-
-model.summary()
-
-identified_estimand = model.identify_effect(proceed_when_unidentifiable=True)
-
-print(identified_estimand)
-
-causal_estimate = model.estimate_effect(identified_estimand,
-        method_name="backdoor.propensity_score_stratification")
-        
-print(causal_estimate)
-
-res_random=model.refute_estimate(identified_estimand, causal_estimate, method_name="random_common_cause", show_progress_bar=True)
-print(res_random)
-
-#%%
-from econml.dml import LinearDML
-
-est = LinearDML()
-
-X = lalonde.loc[:,["age", "married", "educ", "nodegree", "re74", "re75"]].to_numpy()
-
-est.fit(lalonde['re78'], 
-        lalonde['treat'], 
-        X=X
-        )
-point = est.effect(X, T0=0, T1=1)
-
-point = est.const_marginal_effect(X)
-lb, ub = est.const_marginal_effect_interval(X, alpha=0.05)
+print(S_te)
 
 # %%
 
